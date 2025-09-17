ad8aae992cf184dd10181c9447767b8cc0565a31
Yoowatney <>
2025-09-17 07:00:02 +0000

    FEAT: 가을 컨셉 디자인 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 import Ad from './Ad';
 import Style from './App.module.scss';
+import FallingLeaves from './components/FallingLeaves';
 import Header from './components/Header';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <div className={Style.AppContent}>
+      <FallingLeaves />
       <Header />
       <main className={Style.MainContent}>
         <Outlet />
